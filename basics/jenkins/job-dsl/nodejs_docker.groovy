ca3c4387ac94752db3d9d99db4a2e8035b11822a
d-alloc <roei@pcaid.co.il>
2021-05-30 12:12:03 +0300

    Update nodejs_docker.groovy
    Adding build context to dockerbuild

--- basics/jenkins/job-dsl/nodejs_docker.groovy
+++ basics/jenkins/job-dsl/nodejs_docker.groovy
@@ -14,6 +14,7 @@ job('NodeJS Docker example') {
     steps {
         dockerBuildAndPublish {
             repositoryName('dalloc/docker-nodejs-demo')
+            buildContext('../')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
